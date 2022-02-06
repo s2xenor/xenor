20204008f34bddfcfc4c8ac6e164b5e4a1cdde68
Timothy Didierjean <timothy.didierjean@epita.fr>
2022-02-06 11:58:06 +0100

    Added Dialog System

---
+++ DialogSystem/Assets/Prefabs/SETUP.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SETUP : MonoBehaviour
+{
+    /* How to Setup
+     * Put the 3 prefabs in the scene and set a trigger to run the DialogTrigger function on the Dialog Trigger script attached to the trigger
+     */
+}
