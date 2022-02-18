47d0783c0d58edd068fe20ff99ebd90661eada48
Hugueprime <hugueprime@gmail.com>
2022-02-18 01:56:06 +0100

    wire random color position

---
+++ Assets/Enigm/Wires/Scripts/Plug.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plug : MonoBehaviour
+{
+
+    public int nb = -1;
+    public GameObject wire;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    void OnMouseDown()
+    {
+        if (nb != -1)
+        {
+            Destroy(wire);
+        }
+    }
+}
