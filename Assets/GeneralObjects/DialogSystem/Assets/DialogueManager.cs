f54061514251a00faabdde9aff2efc4dc83128f4
timothy.didierjean <timothy.didierjean@epita.fr>
2022-04-18 14:31:08 +0200

    Added monsters + Arranged folders

---
+++ Assets/GeneralObjects/DialogSystem/Assets/DialogueManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	public Animator animator;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue(Dialogue dialogue)
+	{
+		animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name; // Display name
+
+		sentences.Clear(); // Clear previous sentences
+
+		foreach (string sentence in dialogue.sentences) // Enqueue sentences to be said
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0) // If no more sentences to say
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence) // Make cool animation
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
+	}
+
+}
---
+++ Assets/GeneralObjects/Online/CreateAndJoinRooms.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+
+    public InputField createInput;
+    public InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Tim"); // Scene to load
+    }
+}
