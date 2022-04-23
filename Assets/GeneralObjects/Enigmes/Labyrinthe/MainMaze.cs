95392597ca3c09d6264649bacd1b6c8a37d41d69
timothy.didierjean <timothy.didierjean@epita.fr>
2022-04-23 19:51:09 +0200

    Made multiplayer and labyrinthe

--- Assets/GeneralObjects/Enigmes/Labyrinthe/MainMaze.cs
+++ Assets/GeneralObjects/Enigmes/Labyrinthe/MainMaze.cs
@@ -13,17 +13,10 @@ public class MainMaze : MonoBehaviour
     public int line; // 2 * line + 1
     public int column; // 2 * column + 1
 
-    public Transform[] players = new Transform[2]; // Position des 2 joueurs
-    public float[] startPos = new float[2]; // Debut du labyrinthe (format x,y)
-    public int tileSize; // Taille des tiles du labyrinthe
+    public GameObject map;
+    public GameObject mazeContainer;
 
-    int[] player1Tile = new int[2]; // Tile du J1
-    int[] player2Tile = new int[2]; // Tile du J2
-
-    public Transform mapParent;
     public GameObject wall;
-
-    public int[] offsetMap = new int[2];
 
     // Start is called before the first frame update
     void Start()
@@ -35,35 +28,15 @@ public class MainMaze : MonoBehaviour
         List<UInt16[]> gt_output = maze1.GenerateTWMaze_GrowingTree();
         maze = maze1.LineToBlock(); // Convert into array
 
+        // Set up labyrinthe
         ModMaze();
-        Print();
         CreateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
-        player1Tile[0] = (int) (players[0].position.x - startPos[0]) / tileSize; // Calculate position of player in maze
-        player1Tile[1] = (int) (players[0].position.y - startPos[1]) / tileSize;
-        player2Tile[0] = (int) (players[1].position.x - startPos[0]) / tileSize;
-        player2Tile[1] = (int) (players[1].position.y - startPos[1]) / tileSize;
 
-        if (player1Tile[0] >= 0 && player1Tile[0] < maze.GetLength(1) && player1Tile[1] >= 0 && player1Tile[1] < maze.GetLength(0))
-        { // Check if player is in the labyrinth
-            if (maze[player1Tile[1],player1Tile[0]] == 1) // If player is on wall
-            {
-                Debug.Log("Hit");
-                // Damage Player1
-            }
-        }
-
-        if (player2Tile[0] >= 0 && player2Tile[0] < column && player2Tile[1] >= 0 && player2Tile[1] < line)
-        {
-            if (maze[player2Tile[1], player2Tile[0]] == 1)
-            {
-                // Damage Player2
-            }
-        }
     }
 
     void ModMaze() // Modify maze
@@ -87,11 +60,22 @@ public class MainMaze : MonoBehaviour
             {
                 if (maze[i,j] == 1)
                 {
-                    GameObject truc = PhotonNetwork.Instantiate(wall.name, new Vector2(i * size + offsetMap[0], j * size + offsetMap[1]), Quaternion.identity);
-                    // Instantiate wall on network
+                    // Create wall and put it in map childrens
+                    GameObject mur = Instantiate(wall, new Vector2(i * size, j * size), Quaternion.identity);
+                    mur.GetComponent<BoxCollider2D>().enabled = false;
+                    mur.transform.parent = map.transform;
+                    
+                    mur = Instantiate(wall, new Vector2(i * size, j * size), Quaternion.identity);
+                    mur.GetComponent<SpriteRenderer>().enabled = false;
+                    mur.transform.parent = mazeContainer.transform;
                 }
             }
         }
+
+        map.transform.localScale = new Vector2(1, 1);
+        mazeContainer.transform.localScale = new Vector2(2, 2);
+
+        map.SetActive(false);
     }
 
     void Print() // Print maze
