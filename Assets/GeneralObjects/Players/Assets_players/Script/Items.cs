8aa74ed8d0019db77f068aef0deade3ce948cb1a
elya.maury <elya.maury@epita.fr>
2022-04-26 22:17:43 +0200

    final potion/monster

---
+++ Assets/GeneralObjects/Players/Assets_players/Script/Items.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items 
+{
+    public ItemType itemType;
+    public int amount;
+
+    public enum ItemType
+    {
+        PotionStreng,
+        PotionDamage,
+        Health
+    }
+
+    
+
+
+}
