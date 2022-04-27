fb2f4e12c7b0261c20eea7d9cfc55ee10feb26c6
hugo.meens <hugo.meens@epita.fr>
2022-04-27 20:47:22 +0200

    add site

--- site/back/add.php
+++ site/back/add.php
@@ -1,18 +1,21 @@
 <?php
 include('./config.php');
 
-//check if values are present and not empty
+/*
+* User1
+* User2
+* Score
+*/
+
 if(isset($_GET["u1"]) && isset($_GET["u2"]) && isset($_GET["score"]) && !empty($_GET["u1"]) && !empty($_GET["u2"]) && !empty($_GET["score"])){
     $u1 = htmlspecialchars($_GET["u1"]);
     $u2 = htmlspecialchars($_GET["u2"]);
     $score = htmlspecialchars($_GET["score"]);
 
-    //insert in bdd
     $addScore = $bdd->prepare("INSERT INTO scores (user1, user2, score) VALUES (?, ?, ?)");
     $addScore->execute(array($u1, $u2, $score));
     header("HTTP/1.1 200 OK");
 }else{
-    //respond with error
     header("HTTP/1.1 400 Bad Request");
 }
 
