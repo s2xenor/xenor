e6c8f49868bd0a862f7a0487f12db7fc6b94803b
timothy.didierjean <timothy.didierjean@epita.fr>
2022-06-02 19:59:15 +0200

    Small Fix

--- Assets/GeneralObjects/Enigmes/Donjon/Scripts/Destroyer.cs
+++ Assets/GeneralObjects/Enigmes/Donjon/Scripts/Destroyer.cs
@@ -8,6 +8,7 @@ public class Destroyer : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
+		if (other.gameObject.tag == "ClosedRoom")
 		Destroy(other.gameObject); // Destroy room that overlaps with center room
 	}
 }
