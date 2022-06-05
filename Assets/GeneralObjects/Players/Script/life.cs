15905d8600766a3cdcb03ad7ad6fcab8f9768bef
timothy.didierjean <timothy.didierjean@epita.fr>
2022-06-05 19:08:36 +0200

    Fixed le code d'Elya

--- Assets/GeneralObjects/Players/Script/life.cs
+++ Assets/GeneralObjects/Players/Script/life.cs
@@ -15,17 +15,7 @@ public class life : MonoBehaviour //life class
     public  Image cooldown4;
     public bool die = false;
     public float waitTime = 15.0f;
-<<<<<<< HEAD:Assets/GeneralObjects/Players/Script/life.cs
-
-    
-=======
     private GameManager gameManager;
-    // Update is called once per frame
-
-    /*void Update()
-    {    
-    }*/
-
 
     public life(Image heart1, Image heart2, Image heart3, Image heart4, Image heart5)
     {
@@ -40,7 +30,6 @@ public class life : MonoBehaviour //life class
 
 
 
->>>>>>> f9bfac583d019f517885d87d79bd974e1c8d8820:Assets/GeneralObjects/Players/Assets_players/Script/life.cs
     /*
      *Function that swith to another heart canvas 
      */
@@ -67,61 +56,50 @@ public class life : MonoBehaviour //life class
      */
     public void Reduce2(int hearts) //Function that reduce by 1/2 hearts
     {
-<<<<<<< HEAD:Assets/GeneralObjects/Players/Script/life.cs
-
-=======
         gameManager.QuarterHeartLost += hearts*2;
-        if (coolingDown)//Reduce fill amount over the numbers of 1/2 hearts
+
+        float val = 1;//the value which reduces the filAmount 
+        int j = 1;//count of round to switch to the next canvas of heart
+        for (int i = 0; i < hearts; i++)
         {
->>>>>>> f9bfac583d019f517885d87d79bd974e1c8d8820:Assets/GeneralObjects/Players/Assets_players/Script/life.cs
-            float val = 1;//the value which reduces the filAmount 
-            int j = 1;//count of round to switch to the next canvas of heart
-            for (int i = 0; i < hearts; i++)
+            val -= 0.5f;
+            while (cooldown.fillAmount == 0 && j<5)
             {
-                val -= 0.5f;
-                while (cooldown.fillAmount == 0 && j<5)
-                {
-                    cooldown.fillAmount = 0;
-                    cooldown = SwitchImage(j);//Change the image of heart when the filAmount is at 0
-                    j++;
-                    val = 1;
-                }
-                
-             cooldown.fillAmount -= val;
-                
+                cooldown.fillAmount = 0;
+                cooldown = SwitchImage(j);//Change the image of heart when the filAmount is at 0
+                j++;
+                val = 1;
             }
-        
+            
+            cooldown.fillAmount -= val;
+            
+        }
     }
     /*
      *Function that reduce over the number of 1/4
      */
 
-    public  void Reduce4(int hearts) //Function that reduce by 1/4 hearts
+    public void Reduce4(int hearts) //Function that reduce by 1/4 hearts
     {
-<<<<<<< HEAD:Assets/GeneralObjects/Players/Script/life.cs
-        
-=======
         gameManager.QuarterHeartLost += hearts;
-        if (coolingDown)//Reduce fill amount over the numbers of 1/4 hearts
+
+        float val = 1;//the value which reduces the filAmount 
+        int j = 1;//count of round to switch to the next canvas of heart
+        //Reduces by a number of 1/2 heart the filAmount
+        for (int i = 0; i < hearts; i++)
         {
->>>>>>> f9bfac583d019f517885d87d79bd974e1c8d8820:Assets/GeneralObjects/Players/Assets_players/Script/life.cs
-            float val = 1;//the value which reduces the filAmount 
-            int j = 1;//count of round to switch to the next canvas of heart
-            //Reduces by a number of 1/2 heart the filAmount
-            for (int i = 0; i < hearts; i++)
+            val -= 0.25f;
+            while (cooldown.fillAmount == 0 && j < 5)
             {
-                val -= 0.25f;
-                while (cooldown.fillAmount == 0 && j < 5)
-                {
-                    cooldown.fillAmount = 0;
-                    cooldown = SwitchImage(j);//Change the image of heart when the filAmount is at 0
-                    j++;
-                    val = 1;
-                }
-                
-                    cooldown.fillAmount -= val;
-                
+                cooldown.fillAmount = 0;
+                cooldown = SwitchImage(j);//Change the image of heart when the filAmount is at 0
+                j++;
+                val = 1;
             }
+            
+                cooldown.fillAmount -= val;
+            
+        }
     }
 
     //Make the filAmount of all the canvas to 1 
