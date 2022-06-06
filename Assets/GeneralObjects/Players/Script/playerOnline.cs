c7e5d61c2ca1af3a0f5fd877e6a760aa3ec6ab4c
timothy.didierjean <timothy.didierjean@epita.fr>
2022-06-06 21:09:03 +0200

    Potions working in multi

--- Assets/GeneralObjects/Players/Script/playerOnline.cs
+++ Assets/GeneralObjects/Players/Script/playerOnline.cs
@@ -24,48 +24,64 @@ public class playerOnline : MonoBehaviour
 
 
     //life barre of the player 
-    public life vie;
+    life vie;
 
     PhotonView view;
 
     AudioSource audioSource;
     public AudioClip pickup;
-       
+
+
+    public bool boy;
+
+    //damage potion prefab use by the player 
+    public GameObject potionPrefab;
+
+
+    void Start()
+    {
+        inventaire = transform.GetComponentInChildren<inventoryOnline>();//retrieve the prefab of the inventory 
+        transform.GetComponentInChildren<life>();// create life bar
+    }
+
 
     void Awake()
     {
-        inventaire = GetComponentInChildren<inventoryOnline>();
         vie= new life(cooldown, cooldown1, cooldown2, cooldown3, cooldown4);
         view = GetComponent<PhotonView>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D colision)
     {
-        if (collision.gameObject.tag.Contains("slot") && view.IsMine)
+        if (colision.gameObject.tag.Contains("slot") && view.IsMine)
         {
             audioSource.clip = pickup;  
             audioSource.Play();
         }
 
-        switch (collision.gameObject.tag)
+        switch (colision.gameObject.tag)//take the tag of the object
         {
-            case "slot"://strength
+            case "slot"://strength potion 
                 inventaire.slot[0] += 1;
-                inventaire.UpdateNumber(0, inventaire.slot[0].ToString());
-                Destroy(collision.gameObject);
+                inventaire.UpdateNumber(0, inventaire.slot[0].ToString());//add to the inventory 
+                Destroy(colision.gameObject);// destroy the GameObject of the scene
                 break;
 
-            case "slot (1)"://damage
+            case "slot (1)"://damage potion 
                 inventaire.slot[1] += 1;
                 inventaire.UpdateNumber(1, inventaire.slot[1].ToString());
-                Destroy(collision.gameObject);
+                Destroy(colision.gameObject);
                 break;
 
-            case "slot (2)"://health
+            case "slot (2)"://health potion 
                 inventaire.slot[2] += 1;
                 inventaire.UpdateNumber(2, inventaire.slot[2].ToString());
-                Destroy(collision.gameObject);
+                Destroy(colision.gameObject);
+                break;
+
+            case "Monster":
+                colision.gameObject.GetComponent<MonstreOnline>().GetDamage((float)(Strength / (Strength - 1)));//make damage on monster 
                 break;
         }
     }
@@ -73,39 +89,66 @@ public class playerOnline : MonoBehaviour
 
     void Update()
     {
-        /*if (GameObject.Find("Image").GetComponent<image>().PotionStrength)
+        if (GameObject.Find("Image").GetComponent<imageOnline>().PotionHealth)//Strength potion 
         {
-            Strength = Strength * 1.2;
-            GameObject.Find("Image").GetComponent<image>().PotionStrength = false;
+            view.RPC("HealRPC", RpcTarget.All);
         }
-        if (GameObject.Find("Image(2)").GetComponent<image>().PotionHealth)
+        if (GameObject.Find("Image(1)").GetComponent<imageOnline>().PotionDamage)//damage potion
         {
-            Potion potion1 = new Potion(Potion.Type.Heal, 5);
-            potion1.Effect(this, null, vie); 
-            GameObject.Find("Image(2)").GetComponent<image>().PotionHealth = false;
-        }*/
-        if(vie.die)
-        {
-            animator.SetFloat("die", 0);
+            PhotonNetwork.Instantiate(potionPrefab.name, transform.position, Quaternion.identity);
+
+            GameObject.Find("Image(1)").GetComponent<imageOnline>().PotionDamage = false;//remove the object of the inventory
         }
-        if(!vie.die)
+        if (GameObject.Find("Image(2)").GetComponent<imageOnline>().PotionStrength)// health potion 
         {
-            animator.SetFloat("Die", 0);
+            view.RPC("StrengthRPC", RpcTarget.All);
         }
-        if(vie.die)
+        if (!vie.die)
         {
-            animator.SetFloat("Die", 1);
+            animator.SetFloat("Die", 0);//Remove the die animation 
         }
-        StartCoroutine(waiter());
+        if (vie.die)
+        {
+            animator.SetFloat("Die", 1);//make the die animation 
+        }
 
     }
 
+
+    /*
+     *function that wait 30 and put back Strength to 15
+     */
     IEnumerator waiter()
     {
         if (Strength > 15)
         {
             yield return new WaitForSeconds(30);
-            Strength = 15;
+            Strength /= 1.2f;
+
+            if (Strength < 15)
+                Strength = 15;
         }
+    }
+
+
+    public void GetDamage()//make damage on the player 
+    {
+        vie.Reduce2(1); //Reduce 1/2 of the life bar
+    }
+
+    [PunRPC]
+    void StrengthRPC()
+    {
+        Strength = Strength * 1.2;//increase the Strength
+        GameObject.Find("Image(2)").GetComponent<imageOnline>().PotionStrength = false;//remove the object of the inventory 
+        StartCoroutine(waiter());//make wait 30 before put back Strength to 15
+    }
+
+    [PunRPC]
+    void HealRPC()
+    {
+        PotionOnline potion1 = new PotionOnline(PotionOnline.Type.Heal, 5);
+        potion1.Effect(this, vie); //heal the palyer
+        GameObject.Find("Image").GetComponent<imageOnline>().PotionHealth = false;
     }
 }
